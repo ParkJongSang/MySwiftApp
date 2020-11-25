891782f3b1b0bb4b44a3aa4b0c19831ce20d7554
jongsang-park <jongsang.park@webtoonscorp.com>
2020-11-25 17:10:33 +0900

    네비게이션 뷰 추가

--- SwiftUIPrac/ContentView.swift
+++ SwiftUIPrac/ContentView.swift
@@ -19,14 +19,36 @@ struct CrazyView: View {
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
+
+        NavigationView {
             VStack {
-                LoadingView()
-                Text("#LoadingView")
-                    .font(.headline)
+                NavigationLink(destination: CrazyView().frame(width: 200, height: 200)) {
+                    Text("Crazy View")
+                }
+                .navigationTitle("라이트닝 뷰")
+                .padding(.all, 10)
+
+                NavigationLink(destination: LoadingView()) {
+                    Text("Loading View")
+                }
+                .navigationTitle("로딩 뷰(아이패드 air)")
+                .padding(.all, 10)
             }
         }
-        .frame(maxWidth: .infinity,maxHeight: .infinity)
-        .background(Color.white)
+//        ZStack {
+//            VStack {
+//                LoadingView()
+//                Text("#LoadingView")
+//                    .font(.headline)
+//            }
+//        }
+//        .frame(maxWidth: .infinity,maxHeight: .infinity)
+//        .background(Color.white)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
